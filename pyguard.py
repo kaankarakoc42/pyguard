583bdd6ba9f93a260994786e0e495d21fc5f1107
kaan karakoç <56826739+kaankarakoc42@users.noreply.github.com>
2021-02-16 23:00:45 +0300

    Update pyguard.py
    multi language support

--- pyguard.py
+++ pyguard.py
@@ -1,44 +1,112 @@
 from sys import argv
-from os import getcwd,path
-from subprocess import Popen as run
+from os import getcwd,path,remove
+from subprocess import Popen,PIPE,run
 from keyboard import is_pressed
 from colorama import Fore,init
 from time import sleep
 
 init()
 
-def pyguard(file):
-    if not path.exists(getcwd()+"/"+file):
-       print(Fore.RED+"[-] cant find file"+Fore.WHITE)
-       return 
-    a=run(f"python {getcwd()}/{file}")
-    while 1:
-        sleep(0.1)
-        if is_pressed("ctrl+c"):
-            run.kill(a)
-        if is_pressed("ctrl+s"):
-            run.kill(a)
-            print(Fore.GREEN+"[+] restarting script"+Fore.WHITE)
-            a=run(f"python {getcwd()}/{file}")
-            sleep(0.1)
+class pyguard:
+   def __init__(self):
+       self.selections={"python":"py","javascript":"js","c++":"cpp"}
+       self.runtimes={"py":"python","js":"node","cpp":"g++"}
+       self.selected=self.selections["python"]
+       self.run=None
+       self.proccess=None
+       self.parse()
+
+   def parse(self):
+       if len(argv) > 1:
+          if len(argv[1].split("."))>1:
+             if argv[1].split(".")[1]!=self.selected:
+                if argv[1].split(".")[1] in self.selections.values():
+                   self.selected=argv[1].split(".")[1]
+                   self.listen(argv[1].split(".")[0]+"."+self.selected)
+                else:
+                   return print(Fore.RED+"[-] you cant use that extension"+Fore.WHITE)
+             else:
+                self.listen(argv[1])
+          else:
+             self.listen(argv[1]+"."+self.selected)
+       else:
+          return print(details)
+   
+
+   def runFile(self,file):
+       if self.proccess:
+          Popen.kill(self.proccess)
+       if not path.exists(getcwd()+"/"+file):
+          print(Fore.RED+"[-] cant find file"+Fore.WHITE)
+          exit()
+       self.run(file)
+       sleep(0.1)
+
+   def runScript(self,file):
+       self.proccess=Popen(f'{self.runtimes[self.selected]} "{getcwd()}/{file}"')
+   
+   def runCpp(self,file):
+       if self.proccess:
+          Popen.kill(self.proccess)
+       if path.exists(f"{getcwd()}/"+file.split(".")[0]+".exe"):
+          remove(f"{getcwd()}/"+file.split(".")[0]+".exe")
+       self.proccess=Popen(f'{self.runtimes[self.selected]} "{getcwd()}/{file}" -o "{getcwd()}/{file.split(".")[0]}.exe"')
+       while not path.exists(f"{getcwd()}/"+file.split(".")[0]+".exe"):
+             pass
+       sleep(0.1)
+       Popen.kill(self.proccess)
+       self.proccess=Popen(f'"{getcwd()}/{file.split(".")[0]}.exe"')
+   
+   def runTimeExists(self,file):
+       if not self.runtimes[file.split(".")[1]] in run(["path"], capture_output=True, text=True,shell=True).stdout:
+          if self.runtimes[file.split(".")[1]]=="g++" and "mingw" in run(["path"], capture_output=True, text=True,shell=True).stdout:
+             pass
+          else:
+             print(Fore.RED+"[-] you dont have "+{self.runtimes[file.split(".")[1]]}+" runTime please install"+Fore.WHITE)
+             exit()
+
+       
+
+   def listen(self,file):
+       print(Fore.GREEN+f"[+] starting {file}"+Fore.WHITE)
+       self.runTimeExists(file)
+       if self.selected != "cpp":
+          self.run=self.runScript
+       else:
+          self.run=self.runCpp
+       self.runFile(file)
+       while 1:
+         sleep(0.1)
+         if is_pressed("ctrl+c"):
+             Popen.kill(self.proccess)
+         if is_pressed("ctrl+s"):
+             print(Fore.GREEN+"[+] restarting script"+Fore.WHITE)
+             self.runFile(file)
 
 
-        
-if __name__=="__main__":
-   if len(argv) == 1:
-      print(Fore.GREEN+"pyguard is a auto starter for python scripts")
-      print(Fore.GREEN+"usage : pyguard [python-file-name]")
-      print(Fore.RED+"\n[tip] press twice ctrl+s for restart" +Fore.WHITE+" \U0001F60E")
-      print(Fore.GREEN+"\nif you like that you can follow my github account for more useful codes"+Fore.WHITE+" \U0001F60D	")
-      print(Fore.GREEN+"https://www.github.com/kaankarakoc42"+Fore.WHITE)
-      exit()
-   try:
-    print(Fore.GREEN+"[+] pyguard has been started"+Fore.WHITE)
-    print(Fore.GREEN+"[+] running script"+Fore.WHITE)
-    if argv[1].endswith(".py"): 
-       pyguard(argv[1])
-    else:
-       pyguard(argv[1]+".py")
-   except:
-    sleep(0.2)
-    print("\n"+Fore.RED+"[-] app has been shutdown"+Fore.WHITE+"\n")
+
+def main():
+    try:
+      pyguard()
+    except KeyboardInterrupt:
+      print(Fore.RED+"[-] session closed"+Fore.WHITE)
+
+
+details="""
+pyguard42
+
+version: 0.0.5
+author: Mevlüt Kaan Karakoç
+github: https://www.github.com/kaankarakoc42/pyguard42
+pypi: https://pypi.org/user/kaankarakoc42/pyguard42
+
+usage: pyguard [file-name]
+suported-langs: [python,javascript,c++]
+suported-extensions: [.py,.js,.cpp]
+
+
+tips: press ctrl+s twice for auto-restart program
+"""
+
+if __name__=="__main__":  
+   main()
